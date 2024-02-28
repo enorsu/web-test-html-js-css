925c6b9997c105f189fd9aac15ab82d76f3bc65f
SotaNorsu <pelaaja538@gmail.com>
2024-02-28 13:22:15 +0200

    Fixed button position randomization
    fixed js
    changed 1 to vbuck-button

--- vbuck-button/script.js
+++ vbuck-button/script.js
@@ -5,13 +5,16 @@ var ani1 = "transform: rotateX(100deg)";
 var ani2 = "transform: rotateX(0deg)";
 var ani3 = "translate: 90px 90px";
 
+var intViewportWidth = window.innerWidth;
+var intViewportHeight = window.innerHeight;
+
 var randomY;
 var randomX;
 
 
 button1.addEventListener("mouseover", function() {
-    randomX = Math.floor(300 + Math.random() * 600);
-    randomY = Math.floor(300 + Math.random() * 600);
+    randomX = Math.floor(Math.random() * intViewportWidth);
+    randomY = Math.floor(Math.random() * intViewportHeight);
     ani3 = "translate: " + randomX + "px "  + randomY + "px";
     button1.style.cssText = ani3;
 });
